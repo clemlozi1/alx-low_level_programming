82b1603f9848fbcadd167d1acde131ef75de72fd
Clement Mlozi <clemlozi@gmail.com>
2022-07-04 15:06:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,14 @@
 #include <unistd.h>
 
 
-
 /**
+ * main - Prints quote to the standard error
  *
- *  * main - Prints quote to the standard error
+ * Return: zero on success
  *
- *   *
- *
- *    * Return: zero on success
- *
- *     *
- *
- *      */
-
+*/
 int main(void)
-
 {
-
-		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-
-			return (1);
-
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }

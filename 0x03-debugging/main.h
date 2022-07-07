3d9c6dd3c96bf02045b7f3db5e2203afaf98ad1a
Clement Mlozi <clemlozi@gmail.com>
2022-07-07 14:13:39 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,12 @@
-#ifndef main_h
+#ifndef MAIN_H
 
-#define main_h
+#define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
 
-#endif /* main.h */
+#endif /* MAIN_H */
 
 int largest_number(int a, int b, int c);
 

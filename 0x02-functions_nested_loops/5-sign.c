85ebae8b227ed1fa93056e0d0d52c641bb10f629
Clement Mlozi <clemlozi@gmail.com>
2022-07-06 18:20:01 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,53 +1,32 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_sign - Entry point to receive chars
  *
- * Return: Always 0.
+ * @n: Receive the character
+ *
+ * Description: Prints the sign of a number.
+ *
+ * Return: 0
  *
 */
 
-int main(void)
+int print_sign(int n)
 
 {
-	int r;
-
-	r = print_sign(98);
-
-	_putchar(',');
-
-	_putchar(' ');
-
-	_putchar(r + '0');
-
-	_putchar('\n');
-
-	r = print_sign(0);
-
-	_putchar(',');
-
-	_putchar(' ');
-
-	_putchar(r + '0');
-
-	_putchar('\n');
-
-	r = print_sign(0xff);
-
-	_putchar(',');
-
-	_putchar(' ');
-
-	_putchar(r + '0');
-
-	_putchar('\n');
-
-	r = print_sign(-1);
-																			_putchar(',');
-
-	_putchar(' ');
-																			_putchar(r + '0');
-																			_putchar('\n');
-																			return (0);
-
+	if (n > 0)
+		{
+			_putchar(43);
+			return (1);
+		}
+		else if (n == 0)
+		{
+		_putchar(48);
+		return (0);
+		}
+	else
+		{
+		_putchar(45);
+		return (-1);
+		}
 }
